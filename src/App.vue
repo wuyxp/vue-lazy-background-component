db91dfa58b76c7301d41d5729263f2ef7787dc77
w ww <956826374@qq.com>
2019-03-25 14:37:12 +0800

    add: 配置好了基本上能看的vue打包过程

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    App
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,37 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
-    
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader',
+          options: {
+
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    open: true,
+    port: 10000
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new cleanWebpackPlugin(),
+    new htmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
 }
